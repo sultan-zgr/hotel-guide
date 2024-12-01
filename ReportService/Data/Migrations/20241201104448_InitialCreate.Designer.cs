d62ad400d656f3078f04e005c4691190f2503a84
Sultan Özgür <sultan-ozgur@hotmail.com>
2024-12-01 14:20:54 +0300

    fix: Database/ Dockerfile and migrations

--- ReportService/Data/Migrations/20241201104448_InitialCreate.Designer.cs
+++ ReportService/Data/Migrations/20241201104448_InitialCreate.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ReportService.Data;
 
 #nullable disable
 
-namespace ReportService.Migrations
+namespace ReportService.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241201104448_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
